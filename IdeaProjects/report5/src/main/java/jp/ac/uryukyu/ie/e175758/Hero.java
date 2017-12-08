698f15e1fba2020db72878eea9bf285ab06da536
Sho Yoshida <e175758@ie.u-ryukyu.ac.jp>
2017-12-08 16:24:06 +0900

    firstcommit

---
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175758/Hero.java
@@ -0,0 +1,42 @@
+package jp.ac.uryukyu.ie.e175758;
+
+/**
+ * ヒーロークラス。
+ *  String name; //ヒーローの名前
+ *  int hitPoint; //ヒーローのHP
+ *  int attack; //ヒーローの攻撃力
+ *  boolean dead; //ヒーローの生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+
+public class Hero extends LivingThing {
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name ヒーロー名
+     * @param maximumHP ヒーローのHP
+     * @param attack ヒーローの攻撃力
+     */
+
+    public Hero (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+        this.setName(name);
+        setHitPoint(maximumHP);
+        this.setAttack(attack);
+        setDead(false);
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+
+    @Override
+    public void wounded(int damage){
+        setHitPoint(getHitPoint() - damage);
+        if ( getHitPoint() <= 0) {
+            setDead(true);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
+        }
+    }
+}
